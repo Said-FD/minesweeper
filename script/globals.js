7733a978788e0732545c7256331061982d8a4a9d
Maksym Konoval <frontendmax@gmail.com>
2025-08-24 17:30:27 +0200

    Improve signup flow (#1)
    Improve signup flow.
    Add stats update.

--- script/globals.js
+++ script/globals.js
@@ -1,12 +1,18 @@
-const SUPABASE_URL = 'https://tihtcfbjmlklbtvrhbyq.supabase.co'
-const SUPABASE_PUBLIC_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpaHRjZmJqbWxrbGJ0dnJoYnlxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyNTg0OTIsImV4cCI6MjA3MDgzNDQ5Mn0.L5fMYHl1Fuf5CV1GywPKUMiIAZSSymqxcSXdZA7cWJg'
+const SUPABASE_URL = 'https://nmrprnvjxmkqgaeeemfx.supabase.co'
+const SUPABASE_PUBLIC_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tcnBybnZqeG1rcWdhZWVlbWZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwMjYwNzgsImV4cCI6MjA3MTYwMjA3OH0.HD4E8kNgCyMQXBEyHEQIojULlhlACempzCiMPDnatzQ'
+
 const LS_STATS_KEY = 'sb-poopsweeper-stats'
 const LS_STATS_DISPLAY_KEY = 'sb-poopsweeper-stats-display'
 const LS_BOARD_SIZE_KEY = 'boardSize'
 
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_PUBLIC_ANON_KEY)
+const SIGNUP_TYPE = 'signup'
+const LOGIN_TYPE = 'login'
+const WIN_TYPE = 'win'
+const LOSE_TYPE = 'restart'
 
 let clickCount = 0
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_PUBLIC_ANON_KEY)
 
 const getLsItem = key => {
   const item = localStorage.getItem(key)
@@ -15,4 +21,8 @@ const getLsItem = key => {
 
 const setLsItem = (key, value) => {
   localStorage.setItem(key, JSON.stringify(value))
+}
+
+const removeLsItem = key => {
+  localStorage.removeItem(key)
 }
